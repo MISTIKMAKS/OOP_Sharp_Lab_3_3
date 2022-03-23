ed5a043ff222bd59e21f6c378554ebbc7ba3401e
MISTIKMAKS <mistikmaks80885@gmail.com>
2022-03-23 12:01:08 +0200

    first master commit

---
+++ Lab3_3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_3
+{
+    class CatIsNotGoodException : Exception
+    {
+        public CatIsNotGoodException() { }
+
+        public CatIsNotGoodException(string cat) : base(String.Format("Cat is... Errored?", cat))
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void PatCat(string cat)
+            {
+                if (cat == null)
+                {
+                    throw new ArgumentNullException("Cat is... Gone...");
+                }
+                else if(cat == "error")
+                {
+                    throw new CatIsNotGoodException("Cat is... Errored?");
+                }
+                else 
+                {
+                    Console.WriteLine($"Good boy, {cat}!");
+                    Console.WriteLine("");
+                }
+            }
+            string cat_1 = "Vognezir";
+            string cat_2 = null;
+            string cat_3 = "Paporotelapka";
+            string cat_4 = "error";
+            try {
+                PatCat(cat_1);
+                PatCat(cat_2);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("");
+            }
+
+            try
+            {
+                PatCat(cat_3);
+                PatCat(cat_4);
+            }
+            catch (CatIsNotGoodException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("");
+            }
+            finally
+            {
+                Console.WriteLine("We did amazing job!!!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
